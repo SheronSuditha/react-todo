c04e90347150e12ea24d4294ceca1931171d24d4
Sheron Suditha <suditha@gamer.lk>
2020-08-23 13:17:52 +0530

    QUALITY: code cleanup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
+let start = Math.floor(Math.random() * Math.floor(100));
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ todos, settodos ] = useState([]);
+	const entered_todo = useRef(null);
+	const todoItem = useRef(null);
+	const handle_todoAdd = (e) => {
+		e.preventDefault();
+
+		const todo = entered_todo.current.value;
+		settodos([ ...todos, todo ]);
+	};
+
+	const handleAllDone = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<div className="content">
+			<div className="header">
+				<h1>Got something in mind?</h1>
+				<label>Enter your todo!</label>
+				<div className="todo-input">
+					<input type="text" placeholder="Your Todo" ref={entered_todo} /> <br />
+					<button onClick={handle_todoAdd}>
+						<strong>Drop it!</strong>
+					</button>
+				</div>
+			</div>
+
+			<div className="todo-content">
+				{todos.map((t) => (
+					<div className="todo" ref={todoItem}>
+						<h1>{t}</h1>
+						<div className="control">
+							<button style={{ background: 'red' }} onClick={handleAllDone}>
+								<strong>All done!</strong>
+							</button>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
